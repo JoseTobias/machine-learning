f4e8e9df09474e65dd7f1edc750414991eadc08a
JoseTobias <tobiassouza0@gmail.com>
2018-11-13 08:11:57 -0200

    ajustando print

--- q6.py
+++ q6.py
@@ -48,8 +48,6 @@ pca.components_                              # componentes principais
 pca.explained_variance_ratio_                # EVR
 pca.explained_variance_
 
-print(pca.components_)
 print(EVR)
-print(v_dec)
 
-print('Y = ' + str(pca.components_[:,0].T) + ' X1' + ' + ' + str(pca.components_[:,1].T) + ' X2 ' + str(pca.components_[:,2].T) + ' X3 ' + ' + ' + str(pca.components_[:,3].T) + ' X4')
+print('Y = ' + str(pca.components_[:,0]) + ' X1' + ' + ' + str(pca.components_[:,1]) + ' X2 ' + str(pca.components_[:,2]) + ' X3 ' + ' + ' + str(pca.components_[:,3]) + ' X4')
